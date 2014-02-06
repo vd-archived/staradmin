54cd987c4a2e0b8c6e30f7160b3430fbccf2399b
Vivek Singh <vivek@ubuntu.(none)>
2014-02-06 15:18:48 +0530

    Startup Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Startadmin::Application.config.secret_key_base = '0732419042092ccc04016321f847a59343c619635f396de4d4526dd0144b6ce0efbd37d99d2b87180b7e0db78c92284b34d3134ea58c6d1d695fa74f297d52dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Startadmin::Application.config.session_store :cookie_store, key: '_startadmin_session'
